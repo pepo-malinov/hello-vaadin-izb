4242465a104ac7dbe4b470ec134278cb98fb999e
pepo.malinov <pepo.malinov@gmail.com>
2023-02-12 09:07:28 +0200

    implement samplePersonGrid selection listener

--- src/main/java/uni/pu/fmi/views/helloworld/HelloWorldView.java
+++ src/main/java/uni/pu/fmi/views/helloworld/HelloWorldView.java
@@ -1,5 +1,6 @@
 package uni.pu.fmi.views.helloworld;
 
+
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
@@ -15,6 +16,7 @@ import uni.pu.fmi.data.entity.SamplePerson;
 import uni.pu.fmi.data.service.SamplePersonRepository;
 import uni.pu.fmi.views.MainLayout;
 
+
 @PageTitle("Hello World")
 @Route(value = "hello", layout = MainLayout.class)
 @RouteAlias(value = "", layout = MainLayout.class)
@@ -24,7 +26,9 @@ public class HelloWorldView extends VerticalLayout
     private TextField name;
     private Button sayHello;
 
-    public HelloWorldView(SamplePersonRepository repository) {
+
+    public HelloWorldView(SamplePersonRepository repository)
+    {
         name = new TextField("Въведете вашето име");
         sayHello = new Button("Кажи здравей :)");
         sayHello.addClickListener(e -> Notification.show("Здравей " + name.getValue()));
@@ -44,6 +48,11 @@ public class HelloWorldView extends VerticalLayout
         samplePersonGrid.addColumn(SamplePerson::getLastName).setHeader("Фамилия");
         samplePersonGrid.addColumn(SamplePerson::getDateOfBirth).setHeader("Рожденна дата");
         samplePersonGrid.setItems(repository.findAll());
+        samplePersonGrid.setSelectionMode(Grid.SelectionMode.MULTI);
+        samplePersonGrid.addSelectionListener(sl ->
+                                              {
+                                                  Notification.show("Избрахте " + sl.getAllSelectedItems().size() + " записа.");
+                                              });
         add(samplePersonGrid);
     }
 
